d374a58d43686c4deb73935fd45f0cb5349da773
Alexander Piedra <alexander.piedra@daviestech.org>
2020-10-06 18:03:06 +0000

    EVERYTHING

--- Question2.java
+++ Question2.java
@@ -1,11 +1,18 @@
 /* Activity 1.2 - Question 2 */
-
+import java.util.Scanner;
 
 
 class Question2 {
     public static void main(String[] args) {
 
         /* Write your code here */
+        Scanner scan = new Scanner(System.in);
+        String name1, name2, name3;
 
+        System.out.println("Type 3 names for me pls?");
+        name1 = scan.nextLine();
+        name2 = scan.nextLine();
+        name3 = scan.nextLine();
+        System.out.println("Hello " + name3 + name2 + name1 + ", Thanks m8");
     }
 }
--- Question3.java
+++ Question3.java
@@ -1,11 +1,17 @@
 /* Activity 1.2 - Question 3 */
-
+import java.util.Scanner;
 
 
 class Question3 {
     public static void main(String[] args) {
-
         /* Write your code here */
-
+         Scanner scan = new Scanner(System.in);
+        String name, adjective;
+        System.out.println("What is your name(french accent)?");
+        name = scan.nextLine();
+        System.out.println("What adjective describes yuuu??(french accent)");
+        adjective = scan.nextLine();
+        System.out.println("My name is (in Southern American accent) " + name + ". " + "I'm " + adjective + ".");
+         
     }
 }
